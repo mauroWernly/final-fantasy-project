08922326043356b85d2bc46ab404e1fab9582c37
Mauro <mauro.wernly@gmail.com>
2020-04-17 13:56:54 -0300

    Add basic method to evaluate v-show directive with search input

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img width="500" height="168" alt="Final fantasy logo" class="ff-logo" src="../public/assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -23,6 +23,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+
+img.ff-logo{
+  max-width: 500px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main-block">
+
+    <input class="input-character-search" v-model="characterSearch" type="text" placeholder="Search for any character or class.">
+    <div v-bind:key="character.id" v-for="character in charactersList">
+      <div v-show="evaluateCharacterSearch(characterSearch, character.name)">
+        <p>{{ character.name }}</p>
+        <img class="character-image" v-bind:alt="character.alt" v-bind:src="'./assets/' + character.image">
+      </div>
+    </div>
+    <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
   </div>
 </template>
 
 <script>
+import Characters from '../../public/assets/data/characters.json'
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    characterSearch: String
+  },
+  data(){
+    return{
+      charactersList: Characters
+    }
+  },
+  methods:{
+    evaluateCharacterSearch: function (characterSearch, characterName) {
+      if(characterName.toLowerCase().includes(characterSearch.toLowerCase())){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
   }
-}
+};
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+
 h3 {
   margin: 40px 0 0;
 }
@@ -55,4 +58,34 @@ li {
 a {
   color: #42b983;
 }
+
+.main-block{
+  font-family: 'VT323', monospace;
+}
+
+.input-character-search{
+    font-family: 'VT323', monospace;
+    font-size: 24px;
+    min-width: 350px;
+    line-height: 36px;
+    text-align: center;
+    margin-top: 60px;
+    border: 1px solid black;
+    border-left: transparent;
+    border-top: transparent;
+    border-right: transparent;
+}
+.input-character-search:hover,
+.input-character-search:focus{
+  box-shadow:
+  0px 0px 0px 1.5px rgba(255, 255, 255,1),
+  0px 0px 0px 3px rgba(0,0,0,1);
+  background-color: black;
+  color: white;
+}
+
+.character-image{
+  max-width: 200px;
+}
+
 </style>
